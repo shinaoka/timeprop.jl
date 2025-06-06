c06c244d2e19a84438927a9d188292a2132e27d6
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2025-06-06 15:31:15 +0900

    Clean up

--- src/timeprop.jl
+++ src/timeprop.jl
@@ -1,6 +1,5 @@
 module timeprop
-1
-2
+
 function perform_timeprop(F::Function, tmax::Float64, x0::Float64, a0::Float64, h::Float64)::Tuple{Float64, Float64}
     if tmax <= 0.0
         error("tmax must be positive")
